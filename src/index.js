c570b1a9c4a6f87f43ee62de539fcb736d5c1b42
euss99 <euss99@hotmail.com>
2023-01-25 23:07:10 -0600

    Aplicación de mergeAll

--- src/index.js
+++ src/index.js
@@ -1,37 +1,43 @@
 import { fromEvent } from "rxjs";
-import { map } from "rxjs/operators";
+import { map, mergeAll } from "rxjs/operators";
 
 /*=== Variables === */
 const canvas = document.getElementById("reactive-canvas");
-
 const cursorPosition = {x:0, y:0};
 
 /* === Observables === */
-// Cuando se empieza a presionar el click sobre el canvas.
-const onMouseDown$ = fromEvent(canvas, "mousedown").pipe(
-    map(event => {
-        cursorPosition.x = event.clientX - canvas.offsetLeft; // Posición en X.
-        cursorPosition.y = event.clientY - canvas.offsetTop; // Posición en Y.
-        console.log(cursorPosition);
-    })
+const onMouseDown$ = fromEvent(canvas, "mousedown"); // Cuando se empieza a presionar el click sobre el canvas.
+const onMouseMove$ = fromEvent(canvas, "mousemove"); // Cuando se empieza a arastrar el mouse.
+const onMouseUp$ = fromEvent(canvas, "mouseup"); // Cuando se deja de sostener el click sobre el mouse.
+/* A través de mergeAll() empezamos a enviar los enventos de onMouseMove$ mapeados, para luego enviarlos en un observable
+de salida al observador paintStroke */
+const startPaint$ = onMouseDown$.pipe(
+    map(() => onMouseMove$),
+    mergeAll()
 );
-
-// Cuando se empieza a arastrar el mouse.
-const onMouseMove$ = fromEvent(canvas, "mousemove");
-
-// Cuando se deja de sostener el click sobre el mouse.
-const onMouseUp$ = fromEvent(canvas, "mouseup"); 
-
-/* === Suscripciones === */
-onMouseDown$.subscribe();
 
 /* === Características del canvas para hacer una línea === */
 const canvasContext = canvas.getContext("2d"); // Objeto que permite gráficar sobre todo el canvas.
 canvasContext.lineWidth = 4; // Ancho de línea.
 canvasContext.strokeStyle = "white"; // Color de línea.
 
-canvasContext.beginPath(); // Empezar un trazo.
-canvasContext.moveTo(0,0); // Coordenadas donde se empiezan a trazar sobre la pizarra.
-canvasContext.lineTo(100,100); // Cordenada donde termirá el trazo.
-canvasContext.stroke(); // Declarar que es una línea.
-canvasContext.closePath(); // Terminar un trazo.
+const updateCursorPosition = (event) => {
+    cursorPosition.x = event.clientX - canvas.offsetLeft; // Posición en X.
+    cursorPosition.y = event.clientY - canvas.offsetTop; // Posición en Y.
+};
+
+/* El método painStroke nos permitirá dibujar una línea obteniendo las posiciones del cursos (cursosPosition) a la vez, 
+mientras el usuario mueve el cursos actualizamos esa posición. */
+const paintStroke = (event) => {
+    canvasContext.beginPath(); // Empezar un trazo.
+    canvasContext.moveTo(cursorPosition.x, cursorPosition.y); // Coordenadas donde se empiezan a trazar sobre la pizarra.
+    updateCursorPosition(event);
+    canvasContext.lineTo(cursorPosition.x, cursorPosition.y); // Cordenada donde termirá el trazo.
+    canvasContext.stroke(); // Declarar que es una línea.
+    canvasContext.closePath(); // Terminar un trazo.
+};
+
+/* === Suscripciones === */
+onMouseDown$.subscribe();
+onMouseDown$.subscribe(updateCursorPosition);
+startPaint$.subscribe(paintStroke);
